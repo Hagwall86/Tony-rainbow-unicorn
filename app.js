4c7951fa6cdc934dc22fbea482b838699acbfad7
Tony Hagwall <Hagwall86@live.se>
2023-01-16 11:44:14 +0100

    add objekt Nisse

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 "use strict";
 const djur = ["apa", "oragutang", "gorilla", "get", "giraff"];
 console.log(djur);
+// Skapa ett objekt med tre egenskaper. En sträng, en siffra och en boolean. Skriv ut objektet till console och testkör.
+const animalObj = {
+    name: "Nisse",
+    legs: 4,
+    tail: true,
+};
+console.log(animalObj);
--- app.ts
+++ app.ts
@@ -2,3 +2,18 @@
 const djur : string[] = ["apa", "oragutang", "gorilla", "get", "giraff"]
 
 console.log(djur)
+
+// Skapa ett objekt med tre egenskaper. En sträng, en siffra och en boolean. Skriv ut objektet till console och testkör.
+
+
+
+
+
+
+const animalObj: {name: string, legs: number, tail: boolean} = {
+    name: "Nisse",
+    legs: 4,
+    tail: true,
+}
+
+console.log(animalObj)
